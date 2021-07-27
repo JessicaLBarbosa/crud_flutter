0ffce4bb7b05f5928a69d3cd10fb159a8c67bcb5
Jesse <lbarbosa.jessica@gmail.com>
2021-07-27 14:35:10 -0300

    criando os métodos

--- crud_app/lib/provider/users_provider.dart
+++ crud_app/lib/provider/users_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:crud_app/data/dummy_users.dart';
 import 'package:crud_app/models/user.dart';
 import 'package:flutter/material.dart';
@@ -12,5 +14,60 @@ class UsersProvider with ChangeNotifier {
 
   int get count {
     return _items.length;
+  }
+
+  User byIndex(int i) {
+    return _items.values.elementAt(i);
+  }
+
+  //CREATE & UPDATE
+  void put(User user) {
+    if (user == '') {
+      return;
+    }
+
+    if (user.id != '' &&
+        user.id.trim().isNotEmpty &&
+        _items.containsKey(user.id)) {
+      _items.update(
+        user.id,
+        (_) => User(
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          avatarUrl: user.avatarUrl,
+        ),
+      );
+    } else {
+      //CREATE
+      final id = Random().nextDouble().toString();
+      _items.putIfAbsent(
+        id,
+        () => User(
+          id: id,
+          name: user.name,
+          email: user.email,
+          avatarUrl: user.avatarUrl,
+        ),
+      );
+    }
+
+    //teste para depois
+    // if (user.id != '' &&
+    //     user.id.trim().isNotEmpty &&
+    //     _items.containsKey(user.id)) {
+    //   _items.update(
+    //       user.id,
+    //       (_) => user);
+    // }
+
+    notifyListeners();
+  }
+
+  void remove(User user) {
+    if (user != '' && user.id != '') {
+      _items.remove(user.id);
+      notifyListeners();
+    }
   }
 }
--- crud_app/lib/views/user_list.dart
+++ crud_app/lib/views/user_list.dart
@@ -1,13 +1,15 @@
-import 'package:crud_app/data/dummy_users.dart';
+import 'package:crud_app/models/user.dart';
+import 'package:crud_app/provider/users_provider.dart';
 import 'package:crud_app/widgets/user_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget {
   // const UserList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final users = {...DUMMY_USERS};
+    final UsersProvider users = Provider.of(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -20,8 +22,8 @@ class UserList extends StatelessWidget {
         ],
       ),
       body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
       ),
     );
   }
